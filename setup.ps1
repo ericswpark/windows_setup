c46f8e70c21ca5b80ca366456c3f29ae1a5410aa
Eric Park <ideamaneric@gmail.com>
2019-11-02 20:49:24 +0800

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,8 +1,8 @@
 # Install chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-# Do not ask for script running permissioons
-choco feature enable -n allowGlobalConfirmation
+# Do not ask for script running permissions
+choco feature enable -n allowGlobalConfirmation -y
 
 # Install essentials
 choco install firefox
