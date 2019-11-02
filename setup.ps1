1438b856778531e5b1efce18418d41cd7140d8b1
Eric Park <ideamaneric@gmail.com>
2019-11-02 20:48:17 +0800

    Add programs and fix execution

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,12 @@
-﻿# Install chocolatey
+# Install chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Do not ask for script running permissioons
+choco feature enable -n allowGlobalConfirmation
 
 # Install essentials
 choco install firefox
+choco install veracrypt
+choco install vlc
+choco install potplayer
+choco install vscode
