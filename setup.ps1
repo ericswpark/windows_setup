9b8ca5aaea59de657f605d26e1a4769658bab116
Eric Park <ideamaneric@gmail.com>
2019-11-02 20:42:50 +0800

    Add script itself

---
+++ setup.ps1
@@ -0,0 +1,5 @@
+﻿# Install chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Install essentials
+choco install firefox
