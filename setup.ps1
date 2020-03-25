bfc3519b1db2ea4a760aa4e59dc5af18a3e07b1f
Eric Park <ideamaneric@gmail.com>
2020-03-26 01:38:31 +0800

    Rearrange file and comment a lot

--- setup.ps1
+++ setup.ps1
@@ -4,24 +4,37 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.We
 # Do not ask for script running permissions
 choco feature enable -n allowGlobalConfirmation -y
 
-# Install essentials
+# Install browsers
 choco install googlechrome
+choco install firefox
+
+# Install mail client
+choco install thunderbird
+
+# Install utilities
 choco install veracrypt
+choco install 7zip
+choco install winscp
+choco install procexp
+
+# Install video players
 choco install vlc
 choco install potplayer
+
+# Install media players
+choco install itunes
+
+# Install developer tools
 choco install vscode
 choco install atom
-choco install 7zip
 choco install notepadplusplus
 choco install git
 choco install nodejs
 choco install putty
 choco install python
-choco install itunes
-choco install thunderbird
-choco install winscp
-choco install firefox
-choco install procexp
 
-# Remove after graduating from school
+# IMPORTANT - Remove after graduating from school
+# Install Office 365 ProPlus (with license)
 choco install office365proplus
+
+
