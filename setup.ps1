cbc8479c8215763917aa23dd527f53e1bbd5b192
Eric Park <ideamaneric@gmail.com>
2020-03-26 01:36:02 +0800

    Add a bunch of programs
    Sidenote: *.install is not required since they are just metapackages. The ones without correctly installs all software in question.

--- setup.ps1
+++ setup.ps1
@@ -11,6 +11,17 @@ choco install vlc
 choco install potplayer
 choco install vscode
 choco install atom
-choco install 7zip.install
-choco install notepadplusplus.install
-choco install git.install
+choco install 7zip
+choco install notepadplusplus
+choco install git
+choco install nodejs
+choco install putty
+choco install python
+choco install itunes
+choco install thunderbird
+choco install winscp
+choco install firefox
+choco install procexp
+
+# Remove after graduating from school
+choco install office365proplus
