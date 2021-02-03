2a010d30e06b0ec507c7f0e30e6c1c6a55dcccbf
Eric Park <ideamaneric@gmail.com>
2021-02-03 19:26:09 +0900

    Change 1Password to Bitwarden

--- setup.ps1
+++ setup.ps1
@@ -13,7 +13,7 @@ choco feature enable -n allowGlobalConfirmation -y
 # EDIT THIS LINE IF YOU WANT TO CUSTOMIZE PACKAGES INSTALLED
 $packages = 'firefox', 'tor-browser',
 'veracrypt', 'cryptomator',
-'1password',
+'bitwarden',
 '7zip.install', 'winscp', 'procexp', 'hwmonitor', 'gpg4win', 'crystaldiskmark', 'crystaldiskinfo',
 'vlc', 'mpv.install',
 'audacity',
