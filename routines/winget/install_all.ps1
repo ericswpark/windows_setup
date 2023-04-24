44a506739c86d80056e566020cc0ae4a8b528af0
Eric Park <me@ericswpark.com>
2023-04-24 21:39:10 +0900

    routines: winget: add study to "all" script

--- routines/winget/install_all.ps1
+++ routines/winget/install_all.ps1
@@ -5,6 +5,7 @@
 & "$PSScriptRoot\install_developer.ps1"
 & "$PSScriptRoot\install_media.ps1"
 & "$PSScriptRoot\install_social.ps1"
+& "$PSScriptRoot\install_study.ps1"
 & "$PSScriptRoot\install_games.ps1"
 & "$PSScriptRoot\install_3d.ps1"
 & "$PSScriptRoot\install_logitech_drivers.ps1"
