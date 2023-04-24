c18f0893016b037d4a29ef4121495aba4f665edf
Eric Park <me@ericswpark.com>
2023-04-24 17:28:14 +0900

    Fix turn off news and interests routine
    If the "IsFeedsAvailable" and "EnableFeeds" registry entries are not
    set, the "ShellFeedsTaskbarViewMode" entry will reset every time the
    script is run by explorer.
    
    There is an alternate method shared by u/Ill_Branch1293 on Reddit where
    the explorer.eye is killed before the value is set, but I don't like
    that approach because, well, it kills Windows Explorer and it might
    interfere with running tasks. If you still want to see it, find it here:
    https://www.reddit.com/r/sysadmin/comments/s1iomh/comment/hw0soe4/

--- routines/turn_off_news_and_interests.ps1
+++ routines/turn_off_news_and_interests.ps1
@@ -1,2 +1,7 @@
 # Turns off the Weather and News icon in the task bar
-Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Feeds" -Name "ShellFeedsTaskbarViewMode" -Value 2
+# Thanks to u/Fallingdamage on Reddit for the source! Find it in the URL below:
+# https://www.reddit.com/r/sysadmin/comments/s1iomh/comment/hw0ubxy/
+
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Feeds" -Name "IsFeedsAvailable" -Type DWord -Value 0
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Feeds" -Name "ShellFeedsTaskbarViewMode" -Type DWord -Value 2
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Feeds" -Name "EnableFeeds" -Type DWord -Value 0
