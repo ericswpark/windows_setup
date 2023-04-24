ef0e024f38d99c156513d2535379cf09e39c8cf9
Eric Park <me@ericswpark.com>
2023-04-24 17:04:26 +0900

    Try alternative winget installation script

--- routines/install_winget.ps1
+++ routines/install_winget.ps1
@@ -1,1 +1,17 @@
-Add-AppXPackage -Path .\MicrosoftDesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+# From https://stackoverflow.com/a/75334942
+
+# Get latest download url
+$URL = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
+$URL = (Invoke-WebRequest -Uri $URL).Content | ConvertFrom-Json |
+        Select-Object -ExpandProperty "assets" |
+        Where-Object "browser_download_url" -Match '.msixbundle' |
+        Select-Object -ExpandProperty "browser_download_url"
+
+# Download
+Invoke-WebRequest -Uri $URL -OutFile "Setup_winget.msix" -UseBasicParsing
+
+# Install
+Add-AppxPackage -Path "Setup_winget.msix"
+
+# Delete file
+Remove-Item "Setup_winget.msix"
