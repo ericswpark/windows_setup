797df78f30ede71d5b50527367ceff85e7c40fa5
Eric Park <me@ericswpark.com>
2023-04-29 18:38:55 +0900

    routines: winget: workaround for VeraCrypt failing to install

--- routines/winget/install_encryption.ps1
+++ routines/winget/install_encryption.ps1
@@ -3,10 +3,13 @@
 # Find programs from https://winget.run/
 
 $Programs = @'
-IDRIX.VeraCrypt
 Cryptomator.Cryptomator
 '@ -split [environment]::NewLine
 
 foreach ($Program in $Programs) {
     Winget install --id $Program
 }
+
+# Workaround for VeraCrypt failing to install
+# See https://github.com/microsoft/winget-pkgs/issues/69241#issuecomment-1211675482
+winget install --id IDRIX.VeraCrypt --exact --source winget --silent
