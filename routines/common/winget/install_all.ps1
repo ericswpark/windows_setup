fc99cd625040981d68f4bd59c2909f5e058528b9
Eric Park <me@ericswpark.com>
2024-08-16 08:34:06 -0400

    winget: all: add drivers

--- routines/common/winget/install_all.ps1
+++ routines/common/winget/install_all.ps1
@@ -4,6 +4,7 @@
 & "$PSScriptRoot\install_blog.ps1"
 & "$PSScriptRoot\install_utility.ps1"
 & "$PSScriptRoot\install_developer.ps1"
+& "$PSScriptRoot\install_drivers.ps1"
 & "$PSScriptRoot\install_media.ps1"
 & "$PSScriptRoot\install_productivity.ps1"
 & "$PSScriptRoot\install_social.ps1"
