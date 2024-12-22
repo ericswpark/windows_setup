dac5f7e4a1b6638ff35916a225922bc1ffcefa20
Eric Park <me@ericswpark.com>
2024-12-22 13:02:06 +0900

    winget: dynamically call all installation scripts

--- routines/common/winget/install_all.ps1
+++ routines/common/winget/install_all.ps1
@@ -1,14 +1,10 @@
-& "$PSScriptRoot\install_browsers.ps1"
-& "$PSScriptRoot\install_encryption.ps1"
-& "$PSScriptRoot\install_auth.ps1"
-& "$PSScriptRoot\install_blog.ps1"
-& "$PSScriptRoot\install_utility.ps1"
-& "$PSScriptRoot\install_developer.ps1"
-& "$PSScriptRoot\install_drivers.ps1"
-& "$PSScriptRoot\install_media.ps1"
-& "$PSScriptRoot\install_productivity.ps1"
-& "$PSScriptRoot\install_social.ps1"
-& "$PSScriptRoot\install_study.ps1"
-& "$PSScriptRoot\install_games.ps1"
-& "$PSScriptRoot\install_3d.ps1"
-& "$PSScriptRoot\install_logitech_drivers.ps1"
+$excludedScripts = @("install_all.ps1", "install_program.ps1")
+
+$scripts = Get-ChildItem -Path $PSScriptRoot -Filter "install_*.ps1" | Where-Object {
+    $_.Name -notin $excludeFiles
+}
+
+foreach ($script in $scripts) {
+    Write-Host "Running $($script.Name)..."
+    & "$PSScriptRoot\$($script.Name)"
+}
