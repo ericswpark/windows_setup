00322e015f8f4b5bced89cab511d3bb5a8bdcab6
Eric Park <me@ericswpark.com>
2024-12-23 18:40:27 +0900

    Add git setup script

---
+++ routines/common/git_setup.ps1
@@ -0,0 +1,8 @@
+# Sets up GitHub with my identity
+# Substitute with your own identity and GPG pubkey!
+
+git config --global user.email "me@ericswpark.com"
+git config --global user.name "Eric Park"
+git config --global commit.gpgsign true
+git config --global user.signingkey "D5DCDB0D"
+git config --global gpg.program "C:\Program Files (x86)\GnuPG\bin\gpg.exe"
