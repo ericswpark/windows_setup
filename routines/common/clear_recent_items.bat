d044f1ec2149485b553ef21c620827adab6bd19e
Eric Park <me@ericswpark.com>
2025-08-31 08:23:06 -0400

    routines: remove Windows version-specific folders
    Same justification as HEAD^

--- routines/common/clear_recent_items.bat
+++
@@ -1,8 +0,0 @@
-:: Delete all recents
-del /F /Q %APPDATA%\Microsoft\Windows\Recent\*
-del /F /Q %APPDATA%\Microsoft\Windows\Recent\AutomaticDestinations\*
-del /F /Q %APPDATA%\Microsoft\Windows\Recent\CustomDestinations\*
-
-:: Restart Windows Explorer
-taskkill /f /im explorer.exe
-start explorer.exe
