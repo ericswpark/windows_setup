9480d139d148582c562d1464b95b5b94d5930ac2
Eric Park <me@ericswpark.com>
2025-10-02 11:43:03 -0400

    routines: winget: windows_fixer: move Open Shell to interactive install
    Open Shell has the Explorer toolbar that I do not need or want. Thus we
    should do an interactive install and manually remove it for now.

--- routines/winget/install_windows_fixers.ps1
+++ routines/winget/install_windows_fixers.ps1
@@ -3,10 +3,13 @@
 # Find programs from https://winget.run/
 
 $Programs = @'
-Open-Shell.Open-Shell-Menu
 '@
 
 $scriptpath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptpath
 . "$dir\install_programs.ps1"
 installPrograms -Caller "windows_fix" -Programs $Programs
+
+# Open-Shell needs to be installed separately as the interactive installer lets
+# you uncheck the Shell Toolbar.
+winget install --exact --interactive --id Open-Shell.Open-Shell-Menu
